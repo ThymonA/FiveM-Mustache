3d25114a5c44358c276002c4f3d94735b96d4e3a
TigoDEV <me@tigodev.com>
2020-04-22 08:09:32 +0200

    fxmanifest.lua added

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Logic-less {{mustache}} templates in FiveM with Lua'
+name 'Mustache for FiveM'
+author 'TigoDevelopment'
+contact 'me@tigodev.com'
+author 'Olivine Labs'
+contact 'projects@olivinelabs.com'
+
+version '1.0.0'
+
+client_scripts {
+    'mustache.lua'
+}
+
+server_scripts {
+	'mustache.lua'
+}
